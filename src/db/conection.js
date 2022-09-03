e4abe37ecd7e95cf512b3454ac9e1aaca7c82a05
SiddheshGovindBhadale <siddheshbhadale@gmail.com>
2022-09-03 18:26:55 +0530

    upload

--- src/db/conection.js
+++ src/db/conection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( `mongodb+srv://Siddhesh:Siddhesh3341@cluster0.cn61z.mongodb.net/Hi?retryWrites=true&w=majority`, {
+mongoose.connect( 
+'mongodb://Siddhesh:Siddhesh3341@cluster0-shard-00-00.cn61z.mongodb.net:27017,cluster0-shard-00-01.cn61z.mongodb.net:27017,cluster0-shard-00-02.cn61z.mongodb.net:27017/Hi?ssl=true&replicaSet=atlas-924d41-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser:true,
 }).then(()=>{
   console.log("connection succesful")
